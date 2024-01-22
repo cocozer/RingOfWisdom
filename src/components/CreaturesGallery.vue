77310ec406d5e320f74a1e00e7f8f2678c9a8e7d
cocozer <corentin.pak@gmail.com>
2024-01-22 16:07:30 +0100

    suppression du dossier components et ajout d'une icone d'onglet au site

---
+++ src/components/CreaturesGallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="creatures-gallery">
+        <MobCard/>
+    </div>
+</template>
+
+<script>
+import MobCard from './MobCard.vue'
+
+export default {
+    name: 'MobsGallery',
+    components: {
+        DogCard
+    }
+}
+</script>
+
+<style></style>
---
+++ src/components/MobCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="mob-card">
+      <img class="picture" src="https://static.wikia.nocookie.net/eldenring/images/a/ab/ER_Godrick_Soldier_w_Sword.png"/>
+      <div>
+        <h2 class="name">Soldier</h2>
+      </div>  
+    </div>
+  </template>
+  
+  <script>
+      export default {
+        name: 'MobCard'
+      }
+  </script>
+  
+  <style scoped>
+      .mob-card {
+        display: flex;
+      }
+  
+      h2 {
+           font-size: 20px;
+      }
+  </style>
